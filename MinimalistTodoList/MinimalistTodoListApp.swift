85e6489c6180bbf96de5554eee3b839fe571a59d
joaobug <joaoenfrance@gmail.com>
2024-04-03 13:14:39 -0300

    Initial Commit

---
+++ MinimalistTodoList/MinimalistTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalistTodoListApp.swift
+//  MinimalistTodoList
+//
+//  Created by Joao Leal on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MinimalistTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
