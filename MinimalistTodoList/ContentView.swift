9a9866a98ccd367fd5f27c8a15065990d979d610
joaobug <joaoenfrance@gmail.com>
2024-04-03 14:21:32 -0300

    This is my number 1 app to go on my portfolio - The content view displays an array of items In  the tool bar there is a button that the action add notes When pressed it toggles a state showing a sheet This sheed the user can add new items to go on its to do list created 2 functions to save, load

--- MinimalistTodoList/ContentView.swift
+++ MinimalistTodoList/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: Array<String> = []
+    @State var showSheet = false
+    @State var textItemTemp = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List((text), id: \.self){ i in
+                Text(i)
+            }
+            .navigationTitle("Notes")
+            .toolbar {
+                Button(action: {
+                    showSheet.toggle()
+                    textItemTemp = ""
+                }, label: {
+                    Image(systemName: "plus")
+                })
+            }
+            .onChange(of: text) {
+                save()
+                load()
+            }
+            
+            .onAppear() {
+                save()
+                load()
+            }
+            
+            .refreshable {
+                save()
+                load()
+            }
+            
+            
+        }.sheet(isPresented: $showSheet){
+            NavigationStack{
+            
+                    List{
+                        TextField("item", text: $textItemTemp)
+                    }
+                    .navigationTitle("Add Item")
+                    .toolbar {
+                        Button("Add"){
+                            text.append(textItemTemp)
+                            showSheet.toggle()
+                        }
+                    }
+                
+            }
         }
-        .padding()
+    }
+    func save() -> Void {
+        let temp = text.joined(separator: "/[split]/")
+        let key = UserDefaults.standard
+        key.set(temp, forKey: "text")
+    }
+    
+    func load() -> Void {
+        let key = UserDefaults.standard
+        let temp = key.string(forKey: "text") ?? ""
+        let temparray = temp.components(separatedBy: "/[split]/")
+        text = temparray
     }
 }
 
